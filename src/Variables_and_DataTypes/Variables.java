e178ff5ee95a2fed168661ddd9de016a2d100e43
eg <eg@gmail.com>
2017-09-19 12:42:03 -0700

    Variables

---
+++ src/Variables_and_DataTypes/Variables.java
@@ -0,0 +1,18 @@
+package Variables_and_DataTypes;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        /* ================ Constants ================
+        * Constants defines by system word 'final'
+        * Constants variable name should be capitalized
+        */
+        final double PI = 3.1415;
+        System.out.println(PI);
+        //PI=5;   ERROR
+
+
+
+    }
+
+}
