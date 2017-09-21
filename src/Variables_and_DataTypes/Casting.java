77b30777da64155640de99fb9fe3f10af9ca82f7
eg <eg@gmail.com>
2017-09-21 12:44:20 -0700

    d

---
+++ src/Variables_and_DataTypes/Casting.java
@@ -0,0 +1,11 @@
+package Variables_and_DataTypes;
+
+public class Casting {
+    public static void main(String[] args) {
+        byte a =10;
+        short b = 0;
+        b = a;
+        System.out.println("b=" + b);
+
+    }
+}
