f7dc9526e83350c27f8071ae8ddbff8ab0af3b82
Yahor <Yahor@tut.by>
2017-09-21 18:33:08 +0300

    Constant

---
+++ src/Variables_and_DataTypes/Constants.java
@@ -0,0 +1,14 @@
+package Variables_and_DataTypes;
+
+public class Constants {
+    public static void main(String[] args) {
+
+        /* ================ Constants ================
+        *  Для указания константы используется служебное слово `final'.
+        *  Имя констант пишется большими буквами.
+        */
+        final double PI = 3.1415;
+        System.out.println("Значение PI=" + PI);
+        //PI=5;   ERROR
+    }
+}
