6766542710c8ddeffd79017d673653fe4232da5e
Yahor <Yahor@tut.by>
2017-09-26 22:55:21 +0300

    Home work 001

---
+++ src/HomeWork/Vvedenie_001/Hello.java
@@ -0,0 +1,8 @@
+//Используя IntelliJ IDEA, создайте проект с классом Hello.java и выполните его через консоль.
+package HomeWork.Vvedenie_001;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("This is Hello class");
+    }
+}
---
+++ src/HomeWork/Vvedenie_001/HelloEg.java
@@ -0,0 +1,10 @@
+//Используя IntelliJ IDEA, создайте проект с классом Hello.java, который выведет в консоль строку «Hello YOUR_NAME».
+//Скомпилируйте его через консоль с помощью команды javac. Выполните скомпилированный файл Hello.class через консоль.
+
+package HomeWork.Vvedenie_001;
+
+public class HelloEg{
+public static void main(String[] args) {
+		System.out.println("Hello Yegor");
+		}
+}
