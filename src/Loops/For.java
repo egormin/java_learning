e2038c3573b58afc5ffeb9b2829e875b5641be65
egormin <neoman2014@gmail.com>
2019-03-31 22:32:03 +0300

    Update For.java

--- src/Loops/For.java
+++ src/Loops/For.java
@@ -2,7 +2,7 @@ package Loops;
 
 public class For {
     public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++){
+        for (int i = 0; i <= 10; i+=2){  // S shagom 2
             System.out.println(i);
         }
 
