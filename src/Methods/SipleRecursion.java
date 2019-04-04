60539f45caa257826025a93d4ea3f6c13b0dfe70
egormin <neoman2014@gmail.com>
2019-04-04 22:51:05 +0300

    Create SipleRecursion.java

---
+++ src/Methods/SipleRecursion.java
@@ -0,0 +1,27 @@
+package Methods;
+
+public class SimpleRecursion {
+
+    static void recursion(int counter){
+        counter--;
+
+        System.out.println("First part of method: " + counter);
+
+        if (counter !=0){
+            recursion(counter);
+        }
+
+        System.out.println("Second part of method: " + counter);
+    }
+
+    public static void main(String[] args) {
+        recursion(3);
+    }
+}
+
+//First part of method:2
+//First part of method:1
+//First part of method:0
+//Second part of method:0
+//Second part of method:1
+//Second part of method:2
