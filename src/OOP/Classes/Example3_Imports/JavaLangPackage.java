f17d595a081120469693557892581303c99ee6b6
egormin <neoman2014@gmail.com>
2019-04-13 15:02:35 +0300

    Create JavaLangPackage.java

---
+++ src/OOP/Classes/Example3_Imports/JavaLangPackage.java
@@ -0,0 +1,14 @@
+package OOP.Classes.Example3_Imports;
+
+// import java.lang.String;  it is not necessary
+
+/*
+Java by default imports only one package:
+import java.lang.*
+ */
+
+public class JavaLangPackage {
+    public static void main(String[] args) {
+        String str = new String();
+    }
+}
