a94071e933193fda93b1b37391c0f17f57f595e5
egormin <neoman2014@gmail.com>
2019-04-15 23:28:45 +0300

    Create Main.java

---
+++ src/OOP/Classes/Constructors/this_usage/Main.java
@@ -0,0 +1,9 @@
+package OOP.Constructors.this_usage;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat myCat = new Cat("Barsik", 5);
+
+        System.out.println("Name: " + myCat.name + ", Age; " + myCat.age);
+    }
+}
